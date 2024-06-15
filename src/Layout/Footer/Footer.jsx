ede3d8cf96a45c7229a1c4681811c69b00901891
M-Tech <you@example.com>
2024-06-15 21:25:34 +0600

    add pages

---
+++ src/Layout/Footer/Footer.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import './Footer.scss';
+import Logo from '../../images/sapfira-light.png';
+import LogoViolet from '../../images/sapfira-violet.png';
+import VK from '../../images/vk.png';
+import WhatsApp from '../../images/whatsapp.png';
+import Telega from '../../images/telegram.png';
+
+function Footer() {
+  const [logoSrc, setLogoSrc] = useState(Logo);
+
+  const handleMouseOver = () => {
+    setTimeout(() => {
+      setLogoSrc(LogoViolet);
+    }, 200);
+  };
+
+  const handleMouseOut = () => {
+    setTimeout(() => {
+      setLogoSrc(Logo);
+    }, 200);
+  };
+
+  return (
+    <div className="footer">
+      <div className='container'>
+        <div className='footer__box'>
+          <div className='footer__left-box'>
+            <img src={logoSrc} alt="Logo" className="footer__logo" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} />
+            <a href="tg://resolve?domain=Lord_Michael_5" className="footer__dev">
+              Developer and creator: Michael-T-King
+            </a>
+          </div>
+          
+          <ul className="nav__list">
+            <li className="nav__items">ГЛАВНАЯ</li>
+            <li className="nav__items">О НАС</li>
+            <li className="nav__items">КОНТАКТЫ</li>
+            <li className="nav__items">ДОСТАВКА</li>
+            <li className="nav__items">ОСТАВИТЬ ЗАЯВКУ</li>
+          </ul>
+
+          <div className="footer__right-box">
+            <div className='soc__box'>
+              <a href="/" className="footer__soc">
+                <img src={VK} alt="VK" className="soc__img" />
+              </a>
+              <a href="/" className="footer__soc">
+                <img src={WhatsApp} alt="WhatsApp" className="soc__img" />
+              </a>
+              <a href="/" className="footer__soc">
+                <img src={Telega} alt="Telegram" className="soc__img" />
+              </a>
+            </div>
+
+            <p className="copyright">SAPFIRA © 2024 Все права защищены.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/Layout/Header/Header.jsx
@@ -0,0 +1,80 @@
+import React, {useState, useEffect, Link } from 'react';
+import './Header.scss';
+import Logo from '../../images/sapfira-light.png';
+import LogoViolet from '../../images/sapfira-violet.png';
+import BlueGuy from '../../images/header-img.png';
+
+const Header = () => {
+  useEffect(() => {
+    const titles = document.querySelectorAll('.header__titles');
+    const img = document.querySelector('.header__box-right');
+
+    const animateTitles = () => {
+      titles.forEach((title, index) => {
+        setTimeout(() => {
+          title.classList.add('animate');
+        }, index * 300);
+      });
+    };
+
+    const animateImg = () => {
+      setTimeout(() => {
+      img.classList.add('animate');
+      }, 500);
+    };
+
+    window.addEventListener('load', () => {
+      animateTitles();
+      animateImg();
+    });
+
+    return () => {
+      window.removeEventListener('load', animateTitles);
+      window.removeEventListener('load', animateImg);
+    };
+  }, []);
+
+  const [logoSrc, setLogoSrc] = useState(Logo);
+
+  const handleMouseOver = () => {
+    setTimeout (()=>{
+    setLogoSrc(LogoViolet);
+    }, 200);
+  };
+
+
+  const handleMouseOut = () => {
+    setTimeout(() => {
+      setLogoSrc(Logo);
+    }, 200);
+  };
+
+  return (
+    <div className="header__box">
+      <div className='container'>
+        <div className="nav">
+          <img src={logoSrc} alt="" className="nav__img" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} />
+          <ul className="nav__list">
+            <li className="nav__items">ГЛАВНАЯ</li>
+            <li className="nav__items">О НАС</li>
+            <li className="nav__items">КОНТАКТЫ</li>
+            <li className="nav__items">ДОСТАВКА</li>
+            <li className="nav__items">ОСТАВИТЬ ЗАЯВКУ</li>
+          </ul>
+        </div>
+        <div className='header__banner-box'>
+          <div className="header__box-left">
+            <h1 className='header__left-title header__titles'>Украшения и сувениры из полимерной глины на заказ,</h1>
+            <h2 className="header__left-giscription header__titles">изготавливаем с любовью и творчеством.</h2>
+            <h2 className="header__left-text header__titles">Исскуство в каждой детали</h2>
+          </div>
+          <div className="header__box-right">
+          <img src={BlueGuy} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
