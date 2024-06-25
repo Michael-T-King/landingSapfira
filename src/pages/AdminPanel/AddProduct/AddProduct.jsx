98492e55f2af0825a6b4fa46151ee8f0eb801a85
M-Tech <you@example.com>
2024-06-25 15:16:35 +0600

    add required feeld

--- src/pages/AdminPanel/AddProduct/AddProduct.jsx
+++ src/pages/AdminPanel/AddProduct/AddProduct.jsx
@@ -1,82 +1,74 @@
 import React, { useState } from 'react';
-import './AddProduct.scss'
+import './AddProduct.scss';
 import axios from "axios";
-import {useSelector, useDispatch} from "react-redux";
-import {setAllProducts} from '../../../redux/Reducer/products.js'
+import { useDispatch } from "react-redux";
+import { setAllProducts } from '../../../redux/Reducer/products.js';
 
 function AddProduct() {
+  const [category, setCategory] = useState('');
+  const [name, setName] = useState('');
+  const [image, setImage] = useState('');
+  const [price, setPrice] = useState('');
+  const [description, setDescription] = useState('');
+  const [avaleble, setAvailable] = useState(false);
+  const [article, setArticle] = useState('');
 
-const [category, setCategory] = useState();
-const [name, setName] = useState();
-const [image, setImage] = useState();
-const [price, setPrice] = useState();
-const [description, setDescription] = useState();
-const [avaleble, setAvaleble] = useState();
-const [article, setArticle] = useState();
+  const dispatch = useDispatch();
 
-const dispatch = useDispatch();
+  const Product = async (event) => {
+    event.preventDefault();
+    const newProduct = {
+      category,
+      name,
+      price,
+      description,
+      avaleble,
+      article,
+      done: false,
+      image
+    };
+    try {
+      await axios.post('http://localhost:8080/products', newProduct);
+      dispatch(setAllProducts(newProduct));
+    } catch (error) {
+      console.log('Ошибка добавления товара', error);
+    }
+  };
 
-const Product = async(event) =>{
-  event.preventDefault();
-  const newProduct = {
-     category,
-     name,
-     price,
-     description,
-     avaleble,
-     article,
-     done: "false",
-     image
-  }
-  try {
-    await axios.post('http://localhost:8080/products', newProduct)
-    dispatch(setAllProducts(newProduct));
-
-  } catch {
-    console.log ('ошибка добавления товара')
-  }  
-};
-
-const handleSubmit = (e) => {
-  e.preventDefault();
-};
-
-
+  const handleImageChange = (e) => {
+    const fileName = e.target.files[0].name;
+    setImage(`/assets/product/${fileName}`);
+  };
 
   return (
     <section className='product'>
-
-<form  onSubmit={handleSubmit} action="" className="add__product-form">
-<select onChange={(e) => setCategory(e.target.value)} className="product__input">
-  <option value="">Выберите категорию товара</option>
-  <option value="braslets">Браслеты</option>
-  <option value="brelok">Брелоки</option>
-  <option value="brosh">Броши</option>
-  <option value="kolie">Колье</option>
-  <option value="magnet">Магниты</option>
-  <option value="kit">Наборы</option>
-  <option value="pendant">Подвески, Кулоны</option>
-  <option value="earring">Серьги</option>
-  <option value="souvenir">Сувениры</option>
-  <option value="any">Разное</option>
-</select>
-
-      <input onChange={(e)=>{setName(e.target.value)}} type="text" className="product__input" placeholder='Название' />
-      <input onChange={(e)=>{setPrice(e.target.value)}} type="text" className="product__input" placeholder='Цена' />
-      <input onChange={(e)=>{setArticle(e.target.value)}} type="text" className="product__input" placeholder='Артикул' />
-      <textarea onChange={(e)=>{setDescription(e.target.value)}} name="" id="" className="product__textarea" placeholder='Описание товара'></textarea>
-      <input onChange={(e) => {const fileName = e.target.value.split('\\').pop(); setImage(`/assets/product/${fileName}`);}} type="file" accept="image/png, image/jpeg" className='add__image'></input>
-    <div className='checkbox__box'>
-        <input onChange={(e) => { setAvaleble(e.target.checked ? "true" : "false") }} type="checkbox" id='product__checkbox' className="product__checkbox"/>
-  
-        <label htmlFor="product__checkbox">в наличии</label>
-    </div>
-      <button onClick={Product} type='submit' className="login__btn margin__btn">Добавить товар</button>
-
-</form>
-    
+      <form onSubmit={Product} className="add__product-form">
+        <select value={category} onChange={(e) => setCategory(e.target.value)} className="product__input" required>
+          <option value="" disabled>Выберите категорию товара</option>
+          <option value="braslet">Браслеты</option>
+          <option value="brelok">Брелоки</option>
+          <option value="brosh">Броши</option>
+          <option value="kolie">Колье</option>
+          <option value="magnet">Магниты</option>
+          <option value="kit">Наборы</option>
+          <option value="pendant">Подвески, Кулоны</option>
+          <option value="earring">Серьги</option>
+          <option value="souvenir">Сувениры</option>
+          <option value="any">Разное</option>
+        </select>
+        <input value={name} onChange={(e) => setName(e.target.value)} type="text" className="product__input" placeholder='Название' required/>
+        <input value={price} onChange={(e) => setPrice(e.target.value)} type="text" className="product__input" placeholder='Цена' required/>
+        <input value={article} onChange={(e) => setArticle(e.target.value)} type="text" className="product__input" placeholder='Артикул' required/>
+        <textarea value={description} onChange={(e) => setDescription(e.target.value)} className="product__textarea" placeholder='Описание товара' required></textarea>
+        <input type="file" accept="image/png, image/jpeg" onChange={handleImageChange} className='add__image' required />
+        <div className='checkbox__box'>
+          <input checked={avaleble} onChange={(e) => setAvailable(e.target.checked? "true": "false")} type="checkbox" id='product__checkbox' className="product__checkbox" required/>
+          <label htmlFor="product__checkbox">в наличии</label>
+        </div>
+        <button type='submit' className="login__btn margin__btn">Добавить товар</button>
+      </form>
     </section>
-  )
+  );
 }
 
-export default AddProduct
+export default AddProduct;
